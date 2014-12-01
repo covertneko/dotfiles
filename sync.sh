c001e022175da9ad22e0812d6f28f9a03570cbb8
Eric Winkelmann <sewinkelmann@gmail.com>
2014-11-30 23:17:50 -0800

    Modified sync script to mirror specified directories so unwanted vim plugins get deleted. Sync script now uses a rules.txt for excludes/protects.

--- sync.sh
+++ sync.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
+## Sync dotfiles to home
 cd "$(dirname "${BASH_SOURCE}")"
+
 git pull origin master
-rsync -avh --no-perms --exclude-from="exclude.txt" $@ . ~
+
+# Sync and delete extraneous files from locations in mirror.txt
+rsync -avhr --no-perms --delete --filter="merge rules.txt" --files-from="mirror.txt" $@ . $HOME
+
+# Sync the rest without deleting
+rsync -avh --no-perms --filter="merge rules.txt" --exclude-from="mirror.txt" $@ . $HOME
