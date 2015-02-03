b596083bd91b81b645f9ece4ce7427d4d67fc996
Eric Winkelmann <sewinkelmann@gmail.com>
2015-02-03 14:55:16 -0800

    Switched to GNU stow for symlinking instead of some ugly bash script

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ Dotfiles
 
 I put my dotfiles on GitHub because I hate using different environments on my laptop and VPS and sshfs is too darn slow for most things.
 
-link.sh creates symlinks in $HOME to all top-level files and directories in the dotfiles directory, ignoring filenames in ignore.txt.
+I just use GNU stow to symlink them exactly like https://github.com/xero/ does.
--- link.sh
+++ link.sh
@@ -1,50 +1,6 @@
 #!/usr/bin/env bash
-# Symlinks all top-level files and directories to $HOME
-DOTFILESDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+# Syncs all configs with stow 
 
-# Files not to link
-IFS=$'\n' read -rd '' -a ignore <<< $(cat ignore.txt)
+cd $(dirname "${BASH_SOURCE[0]}")
 
-function in_array()
-{
-  local array="$1[@]"
-  local value=$2
-  local result=1
-
-  for e in ${!array}; do
-    if [[ $e == $value ]]; then
-      result=0
-      break
-    fi
-  done
-  return $result
-}
-
-function link()
-{
-  local name=$1
-  cd $HOME
-
-  if in_array ignore $name; then
-    echo "Ignoring $name."
-  elif [[ -e $name ]]; then
-    if [[ -L "$HOME/$name" ]] && [[ $(readlink "$HOME/$name") == "$DOTFILESDIR/$name" ]]; then
-      echo "$HOME/$name exists and already links to $DOTFILESDIR/$name"
-    else
-      echo "$HOME/$name exists! Delete $HOME/$name and create symlink?"
-      read choice
-      if [[ $choice =~ ^([yY][eE][sS])|[yY]$ ]]; then
-        echo "Deleting and linking ${name}..."
-        rm -rf $name
-        ln -s $DOTFILESDIR/$name $name
-      fi
-    fi
-  else
-    echo "Linking ${name}..."
-    ln -s $DOTFILESDIR/$name $name
-  fi
-}
-
-for f in $(ls -a | tail -n +3); do
-  link $f
-done
+for i in $(ls -d */); do stow -t $HOME ${i%%/}; done
