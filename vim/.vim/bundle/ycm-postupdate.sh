8c1180e67eb27acf9145b54d7449bf438dd663b7
Eric Winkelmann <sewinkelmann@gmail.com>
2015-02-03 15:38:08 -0800

    Removed excess vim plugins, fixed vim plugin submodules

--- vim/.vim/bundle/ycm-postupdate.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# Properly update YCM by rebuilding the compiled portion
-cd "$(dirname "${BASH_SOURCE}")"
-cd ./YouCompleteMe
-git submodule update --init --recursive
-./install.sh --clang-completer --system-libclang
