5d5047f80e0ee3a53ddda26540fd54c40c8ad061
Eric Winkelmann <sewinkelmann@gmail.com>
2015-02-04 04:54:32 -0800

    Removed zsh because I don't use it anymore

--- zsh/.zsh/aliases.zsh
+++
@@ -1,8 +0,0 @@
-alias dhist='dirs -v'
-
-# BSD or GNU ls???
-if ls --version 2> /dev/null | grep -q "coreutils"; then
-  alias ls='ls --color=always'
-else
-  alias ls='ls -G'
-fi
--- zsh/.zshenv
+++
@@ -1,4 +0,0 @@
-export CLICOLOR=YES
-export EDITOR=vim
-export PATH="$PATH:$HOME/.cabal/bin:."
-export FPATH="$FPATH:$HOME/.zsh/functions"
