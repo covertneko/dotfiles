8b29cc7854814f640b4f0669088a7bb4f3228504
Eric Winkelmann <sewinkelmann@gmail.com>
2015-02-04 04:50:07 -0800

    Moved .xmobarrc to .xmonad/xmobar.hs

---
+++ xmonad/.xmonad/xmobar.hs
@@ -0,0 +1,15 @@
+Config  { font   = "xft:Open Sans:style=Regular:size=11:antialias=true"
+        , bgColor = "#002b36"
+        , fgColor = "#839496"
+        , position = TopW L 90
+        , lowerOnStart = True
+        , commands = [ Run Weather "KVUO" [ "-t", " <tempC>°C <skyCondition>", "-L", "5", "-H", "30", "--normal", "green", "--high", "red", "--low",  "lightblue" ] 36000
+                     , Run MultiCpu [ "-t", "CPU: <total0> <total1> <total2> <total3>", "-L", "30", "-H", "60", "--normal", "green", "--high", "red" ] 10
+                     , Run Memory [ "-t", "Mem: <usedratio>%" ] 10
+                     , Run Swap [] 10
+                     , Run Date "%a, %b %_d, %l:%M" "date" 10
+                     , Run StdinReader
+                     ]
+        , sepChar = "%"
+        , alignSep = "}{"
+        , template = "%StdinReader% }{ %multicpu% | %memory% | %swap%               <fc=#ee9a00>%date%</fc> | %KVUO%"
