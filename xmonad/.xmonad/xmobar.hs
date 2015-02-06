fbb71febf9056ba73a475d44e021f9d097eaf779
Eric Winkelmann <sewinkelmann@gmail.com>
2015-02-06 04:01:45 -0800

    Changed xmobar colours to match with the solarized color scheme

--- xmonad/.xmonad/xmobar.hs
+++ xmonad/.xmonad/xmobar.hs
@@ -1,10 +1,20 @@
-Config  { font   = "xft:Open Sans:style=Regular:size=10:antialias=true"
+Config  { font   = "xft:DejaVu Sans:style=Regular:size=10:antialias=true"
         , bgColor = "#002b36"
         , fgColor = "#839496"
-        , position = TopW L 90
+        , position = TopSize L 90 25
+        , border = BottomB
+        , borderColor = "#002b36"
+        , borderWidth = 0
         , lowerOnStart = True
-        , commands = [ Run Weather "KVUO" [ "-t", " <tempC>°C <skyCondition>", "-L", "5", "-H", "30", "--normal", "green", "--high", "red", "--low",  "lightblue" ] 36000
-                     , Run MultiCpu [ "-t", "CPU: <total0> <total1> <total2> <total3>", "-L", "30", "-H", "60", "--normal", "green", "--high", "red" ] 10
+        , commands = [ Run Weather "KVUO" [ "-t", " <tempC>°C, <skyCondition>"
+                                          , "-L", "5", "-H", "30"
+                                          , "--normal", "#859900"
+                                          , "--high", "#dc322f"
+                                          , "--low",  "#268bd2" ] 36000
+                     , Run MultiCpu [ "-t", "CPU: <total0> <total1> <total2> <total3>"
+                                    , "-L", "30", "-H", "60"
+                                    , "--normal", "#859900"
+                                    , "--high", "#dc322f" ] 10
                      , Run Memory [ "-t", "Mem: <usedratio>%" ] 10
                      , Run Swap [] 10
                      , Run Date "%a, %b %_d, %l:%M" "date" 10
@@ -12,4 +22,4 @@ Config  { font   = "xft:Open Sans:style=Regular:size=10:antialias=true"
                      ]
         , sepChar = "%"
         , alignSep = "}{"
-        , template = "%StdinReader% }{ %multicpu% | %memory% | %swap%               <fc=#ee9a00>%date%</fc> | %KVUO%"
+        , template = "%StdinReader% }{ %multicpu% | %memory% | %swap%               <fc=#fdf6e3>%date%</fc> | %KVUO%"
