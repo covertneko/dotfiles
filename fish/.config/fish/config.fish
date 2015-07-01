c53e0eb097811ae6ece16a3110b138ad04a1c365
Eric Winkelmann <eric@nikelmwann.io>
2015-07-01 16:43:24 -0700

    Set up ssh-agent on login; Track foreground job execution time and notify if the duration is longer than 10 seconds.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,10 +1,22 @@
-# Add paths if they exist
-set paths "$HOME/.gem/ruby/2.2.0/bin" "$HOME/.cabal/bin" "$HOME/bin"
+# Additional user paths
+set -l paths \
+  "$HOME/.gem/ruby/2.2.0/bin" \
+  "$HOME/.cabal/bin" \
+  "$HOME/bin" \
+  "/usr/bin"
+# Commands not to notify for when their jobs run longer than 10 seconds
+set -g silentjobs "vim"
 
+# Add additional user paths
 for p in $paths
   if test -d $p
     set -gx PATH $p $PATH
   end
+end
+
+if status --is-login
+  # Set up ssh-agent on login if it isn't already running
+  ssh_agent_start
 end
 
 set fish_color_cwd green
@@ -22,16 +34,82 @@ set __fish_git_prompt_color_untrackedfiles red
 set __fish_git_prompt_color_upstream white
 set __fish_git_prompt_color_cleanstate green
 
+function fish_title
+  if test "$_" != "fish"
+    # If entering another job, save the name and time for the prompt later.
+    set -g last_job "$_"
+    set -g last_job_start (date +%s)
+  end
+  echo $_ ' '
+  pwd
+end
+
+function before_prompt --on-event fish_prompt
+    # Save the ending time if we're returning to the shell from a job.
+    set -g last_job_end (date +%s)
+end
+
 function fish_prompt
-  # Visualize exit status of the last executed command
-  set last_status $status
+  set -l last_status $status
+
+  # Last job duration.
+  if begin set -gq last_job last_job_start last_job_end
+  and test -n "$last_job"; end
+    # Ensure the job isn't in the "silentjobs" list.
+    for j in $silentjobs
+      if echo "$j" | egrep -q -- "$last_job"
+        set -g last_job_silent
+        break
+      end
+    end
+
+    if not set -gq last_job_silent
+      # Get the full command line of the last job.
+      # This only works in fish 2.2b1+. $history[1] isn't set before fish_prompt
+      # prior to that version :C. Using $last_job for now instead.
+      #set -l last_cmd_line "$history[1]"
+      set last_cmd_line "$last_job"
+
+      # Calculate the duration of the last job.
+      set -g last_job_duration (math $last_job_end-$last_job_start)
+
+      # Notify with snarl if using cygwin and snarl is installed and the last job
+      # took longer than 10 seconds.
+      if test $last_job_duration -gt 10
+        if type heysnarl > /dev/null ^&1
+          heysnarl "notify?title=$last_cmd_line&text=Returned $last_status, took $last_job_duration seconds." > /dev/null ^&1
+        end
+      end
+    else
+      # Erase it if it's set.
+      set -ge last_job_silent
+    end
+
+    # Clean things up between jobs.
+    set -ge last_job
+    set -ge last_job_start
+    set -ge last_job_end
+  end
+
+  # Print last status as failure or success, or time taken for longer commands.
   if test $last_status -gt 0
     set_color red
-    printf '%s' '✗ '
+    if begin set -gq last_job_duration
+    and test $last_job_duration -gt 10; end
+      printf '%s' "$last_job_duration "
+    else
+      printf '%s' '✗ '
+    end
     set_color normal
   else
     set_color green
-    printf '%s' '✓ '
+    if begin set -gq last_job_duration
+    and test $last_job_duration -gt 10; end
+      test $last_job_duration -gt 10
+      printf '%s' "$last_job_duration "
+    else
+      printf '%s' '✓ '
+    end
     set_color normal
   end
 
