9a11263aa46ba093312896f4777ca2b72c3fc2eb
Eric Winkelmann <eric@nikelmwann.io>
2015-10-04 23:17:49 -0700

    Add fish function to get arduino YCM configuration

---
+++ fish/.config/fish/functions/arduino_init_ycm.fish
@@ -0,0 +1,10 @@
+function arduino_init_ycm
+	set -l config "$HOME/lib/platformio/arduino/.ycm_extra_conf"
+
+  if test -e $config
+    cp $config ./
+  else
+    curl -o $config https://gist.github.com/ajford/f551b2b6fd4d6b6e1ef2
+    arduino_init_ycm
+  end
+end
